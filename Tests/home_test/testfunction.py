92976fb3e0f4a88e2ed1ddda9539530c0a2c572b
CHAUVIRE Timothée <TC229401@intra.cea.fr>
2020-06-24 17:20:58 +0200

    Third Commit

--- Tests/home_test/testfunction.py
+++ Tests/home_test/testfunction.py
@@ -9,27 +9,32 @@ are well working.
 """
 from eprload_BrukerBES3T import *
 from eprload_BrukerESP import * 
+
 import matplotlib.pyplot as plt
+import numpy as np
+#############################################################################
+folder = 'C:\\Users\\TC229401\\Documents\\CoreEPRProjectSuite\\Tests\\home_test\\'
+#filename1 = '20190930_TC_20190826_NOSLY173F_1e_t=0s_T60K.par'
+#filename2 = '20200305_NOSLwT_EDNMR_1.DSC'
+filename3 = '20200311_NOSLwt_FSE_SRT_200us_100Shot_2.DSC'
+filename3b = '20200311_NOSLwt_FSE_SRT_200us_100Shot_2.DSC'
 
-folder = 'C:\\Users\\Tim\\Jupyter\\CoreProject_EPRSuite\\Tests\\home_test\\'
-filename1 = '20190930_TC_20190826_NOSLY173F_1e_t=0s_T60K.par'
-filename2 = '20200305_NOSLwT_EDNMR_1.DSC'
-
-y1,abscissa1,par1 = eprload_BrukerESP(folder+filename1)
-y2,abscissa2,par2 = eprload_BrukerBES3T(folder+filename2)
-
-#import numpy as np
-#import scipy.optimize
-#First test datasmooth
+#y1,x1,par1 = eprload_BrukerESP(folder+filename1)
+#y2,x2,par2 = eprload_BrukerBES3T(folder+filename2)
+y3,x3,par3 = eprload_BrukerBES3T(folder+filename3)
+y3mod,x3mod,par3mod = eprload_BrukerBES3T(folder+filename3b)
 #______________________________________________________________________________
-from datasmooth import *
+# from datasmooth import *
 
-window_length = 13
-method1 = 'binom' 
-method2 ='flat'
-method3 = 'savgol'
-method4 = 'test'
-y_smooth12 = datasmooth(y1,window_length,method2)
+# window_length = 13
+# method1 = 'binom' 
+# method2 ='flat'
+# method3 = 'savgol'
+# method4 = 'test'
+# y_smooth12 = datasmooth(y1,window_length,method2)
 
-from automatic_phase import *
-from windowing import *
+# from automatic_phase import *
+# from windowing import *
+#______________________________________________________________________________
+# from fieldmodulation import *
+# ymodreal,ymodimag = fieldmodulation(x3,y3,3.5,1)
