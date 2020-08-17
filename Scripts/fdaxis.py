82b43b967f306eefcd0ffc7350752dac24b4400b
TChauvire <65663371+TChauvire@users.noreply.github.com>
2020-08-17 23:13:48 +0200

    eighth Commit

--- Scripts/fdaxis.py
+++ Scripts/fdaxis.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 def fdaxis(TimeAxis=None,*args,**kwargs):
-    if TimeAxis==None:  
-        raise ValueError('Wrong number of input arguments!')
-    TimeAxis=np.ravel(TimeAxis)
+    if (TimeAxis.shape[0] != np.ravel(TimeAxis).shape[0]):
+        raise ValueError('The file data must be a column vector')
+    else:
+        TimeAxis = np.ravel(TimeAxis)
     dT=TimeAxis[1] - TimeAxis[0]
     N=len(TimeAxis)      
     NyquistFrequency=1/(2*dT)
-    UnitAxis=np.multiply(2/N,np.arange(0,N-1)-np.fix(N/2))
+    UnitAxis=np.multiply(2/N,np.arange(0,N)-np.fix(N/2))
     FreqAxis=NyquistFrequency*UnitAxis
     return FreqAxis
